36b951ebeea5c736dc2c12a49b797001b328e348
Anuragsomwanshi <anuragsomwanshi11@gmail.com>
2024-12-11 09:47:38 +0530

    First-commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./Components/About/About"
+import Home from "./Components/Home/Home"
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Home/>
+      <About/>
+      
+    </div>
+  )
+}
+
+export default App
