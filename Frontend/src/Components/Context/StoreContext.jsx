b1ce4588bcadd72b2b8f1b6770dbbabf3a82ffbd
Anurag Somwanshi <119263610+Anuragsomwanshi@users.noreply.github.com>
2025-02-13 03:50:47 +0530

    Update StoreContext.jsx

--- Frontend/src/Components/Context/StoreContext.jsx
+++ Frontend/src/Components/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) =>{
 
 
     const [cartItems,setCartItems] = useState({});
-    const url = 'https://aababackend.onrender.com'
+    const url = "https://aababackend.onrender.com"
 
     const [token,setToken] = useState("");
 
